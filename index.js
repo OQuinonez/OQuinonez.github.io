0ec725ba9ebec6bfbe437499b55cd95c8215328d
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-12-15 15:51:44 -0600

    Started working with the buttons that are found in the html

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function loadBtns() {
     // id button.click(function(){
     // Hide other id's  use .hide(number---450?) (rest of the page***)
+    $('#btn-project').click(function() {
+        $('#Projects').show(500);
+        console.log('Hello World');
+    });
 }
 
 function main() {
