3d2062753e142cbff6f18c784046bd98765b75b9
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-12-15 14:56:25 -0600

    Made a JS file for the buttons that will be used in the web page

---
+++ index.js
@@ -0,0 +1,10 @@
+function loadBtns() {
+    // id button.click(function(){
+    // Hide other id's  use .hide(number---450?) (rest of the page***)
+}
+
+function main() {
+    loadBtns();
+}
+
+$(main);
